dc96564a46d42694561427df89f33eb51db1b5a8
Thiago Goncalves Silva <1066623@senacminas.edu.br>
2026-02-26 11:51:44 -0300

    criação de novos arquivos

---
+++ aula.py
@@ -0,0 +1,6 @@
+#inicio do codigo
+nome = "produto"
+quantidade = 2
+preco_unitario = 3500.52
+
+print (f"O valor dos 2 notebooks foi de: $ {3500.52 * 2: .1f}")
---
+++ musica.py
@@ -0,0 +1,17 @@
+musica = """
+E nessa loucura de dizer que não te quero
+Vou negando as aparências
+Disfarçando as evidências
+Mas pra que viver fingindo
+Se eu não posso enganar meu coração?
+Eu sei que te amo!
+
+Chega de mentiras
+De negar o meu desejo
+Eu te quero mais que tudo
+Eu preciso do seu beijo
+Eu entrego a minha vida
+Pra você fazer o que quiser de mim
+Só quero ouvir você dizer que sim!
+"""
+print (musica)
