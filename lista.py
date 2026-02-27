b2ff0407ba9d334735c6f585ef121875bf4efb6b
Thiago Goncalves Silva <1066623@senacminas.edu.br>
2026-02-27 11:58:10 -0300

    lista e tupla

---
+++ lista.py
@@ -0,0 +1,12 @@
+lista_nome = ["aline","carlos","fabio","bruno.","thiago","elias","gabriel","lucas",]
+
+print(lista_nome)
+ 
+
+
+
+
+
+
+
+
---
+++ tupla.py
@@ -0,0 +1,3 @@
+tupla = ("thiago" , 19 , '25.000')
+nome , idade , pretensão_salarial = tupla
+print(f"{nome} tem {idade} e sua pretensão salarial e de {pretensão_salarial}")
